a4cd6b43c17667bfbc39f9cf13314afbe88f6a07
Dave Oxley <dave@daveoxley.co.uk>
2014-06-12 13:31:07 +1000

    Generate version info using git.

---
+++ git-status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "git.commit=`git rev-parse --short HEAD`" > git-status.properties
+branch=`git branch 2>/dev/null| sed -n '/^\*/s/^\* //p'`
+if [ "$branch" == "(no branch)" ] || [[ $branch == \(detached\ from* ]]; then
+    echo "git.tag=`git describe --tags`" >> git-status.properties
+    echo "git.branch=" >> git-status.properties
+else
+    echo "git.tag=" >> git-status.properties
+    echo "git.branch=$branch" >> git-status.properties
+fi
+if git diff --quiet 2>/dev/null >&2; then
+    echo "git.dirty=false" >> git-status.properties
+else
+    echo "git.dirty=true" >> git-status.properties
+fi
+
